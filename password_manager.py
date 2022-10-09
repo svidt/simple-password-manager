33b010af1289cf92d5607a0c658bb6951c5942f6
Svidt <79602784+svidt@users.noreply.github.com>
2022-10-09 21:09:17 +0200

    Add files via upload

---
+++ password_manager.py
@@ -0,0 +1,50 @@
+from cryptography.fernet import Fernet
+
+# master_passwd = input("What is the master password? ")
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open("key.key", "wb") as key_file:
+#         key_file.write(key)
+# write_key()
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("::")
+            print("User:\t\t", user, "\nPassword:\t",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "::" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q" or mode == "quit" or mode == "exit":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
